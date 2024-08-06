f8b585e67bc5affa7eb9c544a26b6ed7c30a26cd
GaylordCC <gcarricaballero3@hotmail.com>
2024-08-06 17:17:25 -0500

    Add detach attribute

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
-driver = webdriver.Chrome()
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+options = Options()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+
 driver.get("http://www.python.org")
 assert "Python" in driver.title
 elem = driver.find_element(By.NAME, "q")
@@ -12,4 +21,5 @@ elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
 time.sleep(6)
-driver.close()
+
+# driver.close()
