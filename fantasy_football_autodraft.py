9dad8598e781656b0eb1f6f5077357176cc15d57
GaylordCC <gcarricaballero3@hotmail.com>
2024-08-09 11:10:57 -0500

    Removed unnecessary code

--- fantasy_football_autodraft.py
+++ fantasy_football_autodraft.py
@@ -4,9 +4,6 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 class EspnBrowser:
     browser, service = None, None
 
