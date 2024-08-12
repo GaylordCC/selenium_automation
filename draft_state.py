b0beb4fa24ae38463579df2772655ca4e892d17b
GaylordCC <gcarricaballero3@hotmail.com>
2024-08-12 16:17:29 -0500

    Add DraftState file, with init method

---
+++ draft_state.py
@@ -0,0 +1,8 @@
+class DraftState:
+    def __init__(self, rosters, turns, freeagents, playerjm=None):
+        self.rosters = rosters
+        self.turns = turns
+        self.freeagents = freeagents
+        self.playerJustMoved = playerjm
+
+    
